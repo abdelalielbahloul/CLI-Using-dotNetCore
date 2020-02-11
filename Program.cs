62986e6d6a5f4ff8d44e0762a736250cf6270c6a
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-02-11 11:03:44 +0100

    feat(simpleCli) : install packages and configure the infrastructure of CLI

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SimpleCli
+{
+    internal class Program
+    {
+        private static async Task<int> Main(string[] args)
+        {
+            //Set appsetting.json file to save the configuration in .NET Core.
+            var Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(AppDomain.CurrentDomain.BaseDirectory + "\\appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(Configuration).CreateLogger();
+
+            // Set the host for app startup and lifetime management.
+            var builder = new HostBuilder()
+                        .ConfigureServices((hostContext, services) =>
+                        {
+                            // set the logging by using Serilog
+                            services.AddLogging(config =>
+                            {
+                                config.ClearProviders();
+                                config.AddProvider(new SerilogLoggerProvider(Log.Logger));
+                            });
+
+                            //services.AddHttpClient();
+                        });
+
+                        try
+                        {
+                            //return await builder.RunCommandLineApplicationAsync<SimpleCliClass>(args);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            return 1;
+                        }
+        }
+    }
+}
